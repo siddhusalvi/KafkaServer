05f1a0ba709b032b284d748cb28e4f652fb43598
siddhusalvi <siddheshsalvi525@gmail.com>
2020-06-21 21:19:33 +0530

    ADDED : Data files

--- src/scriptFile.scala
+++
@@ -1,9 +0,0 @@
-
-    val inputJsonDf = spark.readStream
-      .format("kafka")
-      .option("kafka.bootstrap.servers", BROKERS)
-      .option("subscribe", INPUT_TOPIC_JSON)
-      .option("startingOffsets", "earliest") // going to replay from the beginning each time
-      .load()
-      .selectExpr("CAST(value AS STRING)")
-      .select(from_json($"value", structCricket).as("cricket"))
